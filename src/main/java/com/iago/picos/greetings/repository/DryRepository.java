1dd41bff2a1d60ade04e138b37cea3ac89f43968
iagopicos <iagofernandez94@gmail.com>
2021-01-02 12:43:52 +0100

    Modify properties

--- src/main/java/com/iago/picos/greetings/repository/DryRepository.java
+++ src/main/java/com/iago/picos/greetings/repository/DryRepository.java
@@ -1,13 +1,17 @@
 package com.iago.picos.greetings.repository;
 
 import com.iago.picos.greetings.greeting.Greeting;
+import org.springframework.beans.factory.annotation.Value;
+
 public class DryRepository implements GreetingRepository {
 
-    Greeting greeting;
+    @Value("${default.msg:Hello World}")
+    private String defaultMsg;
+    private Greeting greeting;
 
     @Override
     public Greeting getGreetings() {
-        greeting = new Greeting("Hello World");
+        greeting = new Greeting(defaultMsg);
         return greeting;
     }
 }
