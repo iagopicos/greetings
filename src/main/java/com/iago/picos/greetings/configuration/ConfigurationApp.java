dd85e53b8a03e45250915b72405bbfa52b343f37
iagopicos <iagofernandez94@gmail.com>
2021-01-02 11:37:47 +0100

    Add configuration package

--- src/main/java/com/iago/picos/greetings/configuration/ConfigurationApp.java
+++ src/main/java/com/iago/picos/greetings/configuration/ConfigurationApp.java
@@ -1,4 +1,4 @@
-package com.iago.picos.greetings;
+package com.iago.picos.greetings.configuration;
 
 import com.iago.picos.greetings.controller.GreetingsController;
 import com.iago.picos.greetings.repository.DryRepository;
